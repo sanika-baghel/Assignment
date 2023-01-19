a772ae2cedab32bc49c33d4c88db11b7e5fb72d3
Sanika Baghel <sanika.baghel@prorigosoftware.com>
2023-01-19 16:55:14 +0530

    Javascript Exercise Added

---
+++ JavaScript Excersice/public/IfStatement.js
@@ -0,0 +1,4 @@
+var a = 20;
+if (a > 10) {
+    console.log("value of a is greater than 10");
+}  
---
+++ JavaScript Excersice/public/Operators.js
@@ -0,0 +1,40 @@
+//arithmetic operatos
+let x = 24;
+let y = 2;
+let a = x + y;
+console.log(a);
+
+let b = x * y;
+console.log(b);
+
+let c = x - y;
+console.log(c);
+
+let d = x / y;
+let e = x % y;
+console.log(d + "," + e);
+
+let f = 5;
+f++;
+let g = f;
+console.log(g);
+
+let h = 5;
+h--;
+let i = h;
+console.log(i);
+
+// assignmentoperato
+
+x += 5;
+console.log(x);
+x -= 5;
+console.log(x);
+x *= 5;
+console.log(x);
+x /= 5;
+console.log(x);
+x %= 5;
+console.log(x);
+x >>>= 5;
+console.log(x);
