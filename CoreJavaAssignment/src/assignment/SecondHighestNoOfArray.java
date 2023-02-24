1b6a6a9465ce2ec057bda4e28ab8449c01fba9ba
Sanika Baghel <sanika.baghel@prorigosoftware.com>
2023-02-24 17:39:31 +0530

    assignment

---
+++ CoreJavaAssignment/src/assignment/SecondHighestNoOfArray.java
@@ -0,0 +1,25 @@
+//write a program of java to print second largest no of array
+
+package assignment;
+
+public class SecondHighestNoOfArray {
+	static int arr[] = { 20, 10, 30, 50, 60, 55, 28 };
+
+	static int Secondlargest() {
+		int max = arr[0];
+		int max2 = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max2 = max;
+				max = arr[i];
+			} else if (arr[i] > max2) {
+				max2 = arr[i];
+			}
+		}
+		return max2;
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Largest no : " + Secondlargest());
+	}
+}
--- CoreJavaAssignment/src/assignment/SubWithoutOperator.java
+++ CoreJavaAssignment/src/assignment/SubWithoutOperator.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class SubWithoutOperator {
 	static int sub(int a, int b) {
-		for (int i = 1; i <= b; b--) {
+		for (int i = 1; i <= b; i++) {
 			a--;
 		}
-		for (int i = 0; i > b; b++) {
+		for (int i = 0; i > b; i--) {
 			a++;
 		}
 		return a;
@@ -22,7 +22,7 @@ public class SubWithoutOperator {
 		System.out.println("enter no2");
 		no2 = sc.nextInt();
 		int ans = sub(no1, no2);
-		System.out.println("Addition is");
+		System.out.println("Substraction is");
 		System.out.println(ans);
 	}
 }
