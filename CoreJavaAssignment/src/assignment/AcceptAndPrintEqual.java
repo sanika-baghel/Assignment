f7661f13a78acbda022476bf682082392a47a243
Sanika Baghel <sanika.baghel@prorigosoftware.com>
2023-03-02 19:07:45 +0530

    assignment

--- CoreJavaAssignment/src/assignment/AcceptAndPrintEqual.java
+++ CoreJavaAssignment/src/assignment/AcceptAndPrintEqual.java
@@ -5,15 +5,15 @@ import java.util.*;
 
 public class AcceptAndPrintEqual {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.print("enter no1 ");
-		int no1 = in.nextInt();
+		int no1 = sc.nextInt();
 		System.out.print("enter no2 ");
-		int no2 = in.nextInt();
+		int no2 = sc.nextInt();
 		System.out.print("enter no3");
-		int no3 = in.nextInt();
+		int no3 = sc.nextInt();
 		System.out.print("enter no4");
-		int no4 = in.nextInt();
+		int no4 = sc.nextInt();
 
 		if (no1 == no2 && no2 == no3 && no3 == no4) {
 			System.out.println("Numbers are equal");
---
+++ CoreJavaAssignment/src/assignment/CreateNewFile.java
@@ -0,0 +1,35 @@
+// Java Program to create new file using File class
+package assignment;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CreateNewFile {
+	public static void main(String args[]) {
+		CreateNewFile file1 = new CreateNewFile();
+		file1.newFile();
+	}
+
+	public void newFile() {
+		String strPath = "", strName = "";
+
+		try {
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter the file name:");
+
+			strName = br.readLine();
+			System.out.println("Enter the file path:");
+
+			strPath = br.readLine();
+
+			File file1 = new File(strPath + "" + strName + ".txt");
+
+			file1.createNewFile();
+		}
+
+		catch (Exception ex1) {
+		}
+	}
+}
