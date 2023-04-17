c9fc9f5ba36cae58f67b776c7109858adc48272d
Sanika Baghel <sanika.baghel@prorigosoftware.com>
2023-04-17 19:12:50 +0530

    assignment

---
+++ CoreJavaAssignment/src/com/prorigo/FileHandling/AppendFile.java
@@ -0,0 +1,23 @@
+package com.prorigo.FileHandling;
+
+import java.io.*;
+
+public class AppendFile {
+	public static void main(String[] args) {
+		String strFilePath = "D:\\demo.txt";
+
+		try {
+			FileOutputStream fos = new FileOutputStream(strFilePath, true);
+			String strContent = "Text to be appended.";
+			fos.write(strContent.getBytes());
+			fos.close();
+			System.out.println("Content Successfully Append into File...");
+		} catch (FileNotFoundException ex) {
+			System.out.println("FileNotFoundException : " + ex.toString());
+		} catch (IOException ioe) {
+			System.out.println("IOException : " + ioe.toString());
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.toString());
+		}
+	}
+}
---
+++ CoreJavaAssignment/src/com/prorigo/FileHandling/OwnerOfFile.java
@@ -0,0 +1,21 @@
+package com.prorigo.FileHandling;
+
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.Scanner;
+
+public class OwnerOfFile {
+	public static void main(String[] args) throws Exception {
+		Scanner KB = new Scanner(System.in);
+
+		System.out.print("Enter the file path : ");
+		String A = KB.next();
+		Path path = Paths.get(A);
+
+		FileOwnerAttributeView view = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
+
+		UserPrincipal userPrincipal = view.getOwner();
+
+		System.out.println("Owner of the file is :" + userPrincipal.getName());
+	}
+}
